aa96d11083e35e6fba3fcb9d92b2bdf263e9c2b8
kevin quintino <kevin.quintino@fatec.sp.gov.br>
2020-10-24 18:57:00 -0300

    0.0.2

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -1,16 +1,3 @@
-/*gsap.from(".system__help", {
-    opacity: 0,
-    duration: 2.8,
-    y: 30,
-    ease: "Expo.easeInOut",
-});
-
-gsap.from(".system__logo", {
-  opacity: 0,
-  duration: 1.2,
-  y: -30,
-  ease: "Expo.easeInOut",
-});*/
 function paraIndex() {
     window.location="index.html";
 }
