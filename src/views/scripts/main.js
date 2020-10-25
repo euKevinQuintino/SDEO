17069f1ccf32ca3245f07adde036487d5c166cc6
kevin quintino <kevin.quintino@fatec.sp.gov.br>
2020-10-25 15:41:55 -0300

    0.0.2

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -20,17 +20,14 @@ for (i = 0; i < acc.length; i++) {
 window.addEventListener("keydown", function (event) {
   if (event.key !== undefined) {
     if (event.key === ' ' || 'Enter') {
-      //enter ou espaço
       //event.preventDefault();
       document.activeElement.click();
     }
     if (event.key === 'Escape') {
-      //esc
       //event.preventDefault();
       paraIndex();
     }
     if (event.key === 'F1') {
-      //f1
       //event.preventDefault();
       paraPainelAjuda();
     }
